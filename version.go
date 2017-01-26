8bc56ab40a464a263ce806c5668781143c6ca231
Christopher Hall <hsw@bitmark.com>
2017-01-26 15:09:14 +0800

    add versio, update copyright
    Signed-off-by: Christopher Hall <hsw@bitmark.com>

---
+++ version.go
@@ -0,0 +1,11 @@
+// Copyright (c) 2014-2017 Bitmark Inc.
+// Use of this source code is governed by an ISC
+// license that can be found in the LICENSE file.
+
+package getoptions
+
+const (
+	Major   = "1"
+	Minor   = "0"
+	Version = Major + "." + Minor
+)
